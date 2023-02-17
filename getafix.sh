5eebc88c82aa91d047cfd5974a65abbb1edddcb4
benjaminpope <bjspope@gmail.com>
2023-02-17 16:17:13 +1000

    getafix slurm script

---
+++ getafix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=simulation
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=6
+#SBATCH --mem=16G
+#SBATCH --time=0-00:30:00
+
+# Load the necessary modules
+module load anaconda3/5.2.0
+conda activate ladder
+
+# Change to the directory where your Python script is located
+cd /data/uqbpope/Simulated-Universe
+
+# Run your Python script
+python MainSimulationFile.py > simulation_output.txt
